5a46bd48abcccb06ea2f87a62f9d3d2a71c4f4a2
Bl00D4NGEL <kuhlesdominik@gmx.de>
2019-06-20 19:56:30 +0200

    changed taglist endpoints

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import config from './config';
 import Division from './modules/division';
+import fs, { exists } from 'fs';
 
 // Set up the express app
 const app = express();
@@ -105,10 +106,6 @@ app.get('/tagDivisionMembers/:divisionName', (req, res) => {
 	});
 });
 
-app.get('/tagHouseMembers/:houseName', (req, res) => {
-
-});
-
 app.get('/division/:divisionName', (req, res) => {
 	let params = req.params;
 	console.log('Division', params);
@@ -146,6 +143,125 @@ app.get('/mdr', (req, res) => {
 		}
 	});
 });
+
+app.get('/get/divisionNames', (req, res) => {
+	console.log("Get division names");
+	let parser = new MdrParser();
+	var files = fs.readdirSync(parser.dataDirectory)
+		.filter(fn => fn.startsWith('division-'))
+		.map(x => x.match(/division-(.*?)\.json/)[1]);
+	res.send(files);
+});
+
+app.post('/get/tagList', GetTagList);
+
+async function GetTagList(req, res) {
+		console.log("Get taglist");
+		let reqDivisions = req.body.divisions;
+		if(
+			reqDivisions === undefined ||
+			typeof reqDivisions !== 'object' ||
+			reqDivisions.length == 0
+		) {
+			res.send("Invalid division data sent");
+			return;
+		}
+		let output = '';
+		for (let i = 0; i < reqDivisions.length; i++) {
+			let reqDivision = reqDivisions[i];
+			await GetDivision(reqDivision, function(data) {
+				try {
+					let division = new Division(data);
+					output += GenerateTagListForDataAndRoles(division, req.body.roles);
+					if(division.teams.length > 0) {
+						for (let i = 0; i < division.teams.length; i++) {
+							let team = division.teams[i];
+							output += "<h2 style='color: #1a7303'>" + team.name + "</h2>";
+							output += GenerateTagListForDataAndRoles(team, req.body.roles);
+							if(team.rosters.length > 0) {
+								for (let j = 0; j < team.rosters.length; j++) {
+									let roster = team.rosters[j];
+									output += "<h3 style='color: #025a00'>" + roster.name + "</h3>";
+									output += GenerateTagListForDataAndRoles(roster, req.body.roles);
+								}
+							}
+						}
+					}
+					output += "</div>";
+				}
+				catch(ex) {
+					console.error(ex);
+				}
+			});
+		}
+		
+	res.send(output);
+}
+
+function GenerateTagListForDataAndRoles(data, roles) {
+	const roleMap = {
+		"DC": {
+			name: "Division Commander",
+			val: "commanders",
+			prio: 6
+		},
+		"DV": {
+			name: "Division Vice",
+			val: "vices",
+			prio: 5
+		},
+		"TL": {
+			name: "Team Leader",
+			val: "teamLeaders",
+			prio: 4
+		},
+		"2IC": {
+			name: "Second in Charge",
+			val: "2ICs",
+			prio: 3
+		},
+		"RL": {
+			name: "Roster Leader",
+			val: "rosterLeaders",
+			prio: 2
+		},
+		"TM": {
+			name: "Team Member",
+			val: "members",
+			prio: 1
+		},
+		"SUB": {
+			name: "Sub Player",
+			val: "subs",
+			prio: 0
+		}
+	};
+	// Sort role data by priority
+	roles = roles.sort(function(a, b) {
+		return roleMap[b].prio - roleMap[a].prio;
+	});
+
+	let output = '';
+	let template = '<a href="https://di.community/profile/##id##-##name##/" contenteditable="false" data-ipshover="" data-ipshover-target="https://di.community/profile/##id##-##name##/?do=hovercard" data-mentionid="##id##">@##name##</a>&#8203;&#8203;&#8203;&#8203;&#8203;&#8203;&#8203&nbsp';
+	for (let i=0; i < roles.length; i++) {
+		let role = roles[i];
+		let roleConfig = roleMap[role];
+		if (roleConfig !== undefined) {
+			if (data[roleConfig.val] !== undefined) {
+				let vals = data[roleConfig.val];
+				if (vals.length > 0) {
+					output += roleConfig.name + (vals.length > 1 ? 's' : '') + '<br>';
+					for (let j=0; j < vals.length; j++) {
+						let val = vals[j];
+						output += template.replace(/##id##/g, val.id).replace(/##name##/g, val.name);
+					}
+					output += '<br>';
+				}
+			}
+		}
+	}
+	return output;
+}
 
 async function GetDivision(divisionName, callback) {
 	let mdr = new MdrParser();
