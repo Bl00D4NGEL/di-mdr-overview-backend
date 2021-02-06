4220af86495f8e7cbefaba77747185e32f72a12b
BL00D4NGEL <kuhlesdominik@gmx.de>
2021-02-06 23:47:49 +0100

    fixed server creation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import configureExpress from "./modules/utils/configureExpress";
 import setUpMdrFetcher from "./modules/utils/mdrFetcher";
 import diConfig from './modules/config';
+import * as https from "https";
 
 if (diConfig.reloadData) {
     setUpMdrFetcher();
 }
 
 // @ts-ignore
-export default diConfig.serverType.createServer(diConfig.options, configureExpress()).listen(2048);
+export default https.createServer(diConfig.options, configureExpress()).listen(2048);
