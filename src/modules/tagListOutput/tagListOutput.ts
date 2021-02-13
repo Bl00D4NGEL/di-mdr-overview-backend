f90fed2f756a88e20bbfbc0e7834b43636a40b3c
BL00D4NGEL <kuhlesdominik@gmx.de>
2021-02-13 12:58:18 +0100

    wrap json parse in try catch and added catch for log request to hopefully stop the app from crashing / find root cause

--- src/modules/tagListOutput/tagListOutput.ts
+++
@@ -1,5 +0,0 @@
-import {IMemberCollection} from "../member/memberCollection";
-
-export interface ITagListOutput {
-    generate(members: IMemberCollection): string;
-}
--- src/modules/tagListOutput/tagListOutputWeb.ts
+++
@@ -1,8 +0,0 @@
-import {ITagListOutput} from "./tagListOutput";
-import {IMemberCollection} from "../member/memberCollection";
-
-export default class TagListOutputWeb implements ITagListOutput {
-    generate(memberCollection: IMemberCollection): string {
-        return JSON.stringify(memberCollection);
-    }
-}
